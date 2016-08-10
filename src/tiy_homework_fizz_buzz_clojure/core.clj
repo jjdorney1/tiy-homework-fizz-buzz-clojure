c2850aee818f4fb35bdf51aaa8699011fdfba362
jeffreydorney <jjdorney1@gmail.com>
2016-08-10 15:46:48 -0400

    completed fizzbuzz clojure

--- src/tiy_homework_fizz_buzz_clojure/core.clj
+++ src/tiy_homework_fizz_buzz_clojure/core.clj
@@ -1,6 +1,40 @@
 (ns tiy-homework-fizz-buzz-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;(for [x (range 0 13)
+;      y (range 0 13)]
+;  (str x " * " y " = " (* x y)))
+
+(defn -main[])
+
+(defn fizz-buzz [start end]
+      (for [x (range start end)]
+        (cond (= (mod x 15) 0) (println "FizzBuzz")
+              (= (mod x 3) 0) (println "Fizz")
+              (= (mod x 5) 0) (println "Buzz")
+              :else (println x))))
+
+
+
+;(defn echo [word times]
+;  (loop [word word
+;         times times
+;         accumulator ""]
+;    (if (= times 0)
+;      accumulator
+;      (recur word (- times 1) (str accumulator " " word)))))
+;
+
+;old code that didnt work
+;(if (= (mod x 15) 0)
+;  (println "FizzBuzz"
+;           (if (= (mod x 5) 0)
+;             (println "Buzz"
+;                      (if (= (mod x 3) 0)
+;                        (println "Fizz")
+;                        (println x))))))
+
+;(fizz-buzz 1 100)
+
+(-main)
+
+(fizz-buzz 1 101)
